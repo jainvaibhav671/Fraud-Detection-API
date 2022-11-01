913637e19987cc72478568c98791e85b94e30564
cse-vaibhav <jainvaibhav671@gmail.com>
2022-11-01 23:19:10 +0530

    testing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/
 
-# PORT=8000
+PORT=8000
 echo "\
   [server]\n\
   headless = true\n\
@@ -9,7 +9,7 @@ echo "\
   \n\
   " > ~/.streamlit/config.toml
 
-streamlit run frontend/app.py &
+# streamlit run frontend/app.py &
 
 # cd backend/
 # uvicorn app:app &
