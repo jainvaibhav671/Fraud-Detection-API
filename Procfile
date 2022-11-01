d87db51d5ad94a498af455f8afcbdd2093733d69
cse-vaibhav <jainvaibhav671@gmail.com>
2022-11-01 23:52:49 +0530

    testing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh
+web: gunicorn -w 3 -k uvicorn.workers.UvicornWorker app:app
--- setup.sh
+++ setup.sh
@@ -8,5 +8,3 @@ echo "\
   enableCORS = false\n\
   \n\
   " > ~/.streamlit/config.toml
-
-gunicorn -w 3 -k uvicorn.workers.UvicornWorker app:app
